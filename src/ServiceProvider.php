8ff6180ba762fc640ef3ca0ca7ed1d238c02e2de
Jasno.Chen <chenjxlg@163.com>
2020-03-26 23:57:21 +0800

    符合PSR-4标准

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,14 +10,14 @@ class ServiceProvider extends LaravelServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes([__DIR__ . '/../../config/config.php' => config_path('cart.php')]);
-            $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations/');
+            $this->publishes([__DIR__ . '/../config/config.php' => config_path('cart.php')]);
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations/');
         }
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../../config/config.php', 'cart');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'cart');
     }
 
 }
